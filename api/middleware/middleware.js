6121c41a210802ba7f2816e6da84536f5d9b3d30
MaxT6 <maximiliant6@gmail.com>
2022-06-08 16:07:31 -0400

    set up server

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,37 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timeStamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log('logger middleware', `[${timeStamp}] ${method} to ${url}`)
+  // console.log(`${req.method} ${req.url} ${req.timeStamp}`);
+
+  next()
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log('logger middleware')
+  next()
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log('logger middleware')
+  next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log('logger middleware')
+  next()
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+
+}
